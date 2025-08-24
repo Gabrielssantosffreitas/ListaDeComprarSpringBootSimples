9f5672d2f523350670b12767effc165d6e8b525a
Gabriel Santos <gabrielsantos022freitas@gmail.com>
2025-08-24 00:21:20 -0300

    first commit

---
+++ src/main/java/com/gabriel/ListaDeComprar/ListaDeComprarApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ListaDeComprar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeComprarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeComprarApplication.class, args);
+	}
+
+}
